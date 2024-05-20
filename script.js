d4086adbb31f16208c616da7ae4df30c9e09fa79
L1nar <122251664+L1nar@users.noreply.github.com>
2024-05-20 14:37:34 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const price = document.querySelectorAll('.legend__price');
 const list = document.querySelector('.purcashes__list');
-const product = document.querySelectorAll('.editor__form-input');
+const item = document.querySelector('.purcashes__item');
+const input = document.querySelectorAll('.editor__form-input');
 const selectCategory = document.getElementById('selectCategory');
 const btn = document.querySelector('.btn');
 
-btn.addEventListener('click', (event) => {
-    let newItem = document.createElement('li');
-    newItem.textContent = product.value;
-    list.appendChild(newItem);
-});
+function addTask () {
+    let newItem = document.createElement('td');
+    newItem.classList.add('purcashes__td');
+    newItem.textContent = input.textContent;
+    item.appendChild(newItem);
+}
+
+btn.addEventListener('click', addTask); 
