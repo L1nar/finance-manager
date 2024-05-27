1142a74c40fd6a1c7d8dc75c25d7df1f41d9fde6
L1nar <122251664+L1nar@users.noreply.github.com>
2024-05-27 16:45:22 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const category = document.querySelector('#selectCategory');
 const tbody = document.querySelector('.purcashes__list');
 const list = document.querySelector('.purcashes__item');
 const btn = document.querySelector('.btn');
+const modalWindow = document.querySelector('#myModal');
+const closeBtn = document.querySelector('#close');
+const cancelBtn = document.querySelector('#cancel');
+const removeBtn = document.querySelector('#remove');
+const price = document.querySelectorAll('.legend__price');
+const firstPrice = price[0];
+console.log(firstPrice.dataset.price);
 
 btn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -13,12 +20,42 @@ function addTask () {
     let itemName = document.createElement('td');
     let itemPrice = document.createElement('td');
     let options = document.createElement('td');
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'X';
+    deleteBtn.classList.add('delete');
+    deleteBtn.addEventListener('click', () => {
+        modalWindow.style.display = 'block';
+      });
+      closeBtn.addEventListener('click', () => {
+        modalWindow.style.display = 'none'
+      });
+      cancelBtn.addEventListener('click', () => {
+        modalWindow.style.display = 'none'
+      });
+      removeBtn.addEventListener('click', () => {
+        list.removeChild(itemName);
+        list.removeChild(itemPrice);
+        list.removeChild(options);
+        list.removeChild(deleteBtn);
+        modalWindow.style.display = 'none';
+      });
+
     itemName.textContent = inputName.value;
     itemPrice.textContent = inputPrice.value;
-    options.textContent = category.textContent;
-    list.appendChild(itemName);
-    list.appendChild(options);
-    list.appendChild(itemPrice);
+    options.textContent = category.value;
+
+    if (inputName.value === '') {
+      alert('Поле "Товар" обязательно для ввода.')
+    } 
+    else {
+        list.appendChild(itemName);
+        list.appendChild(options);
+        list.appendChild(itemPrice);
+        list.appendChild(deleteBtn);
+    }
+    inputName.value = '';
+    inputPrice.value = '';
 }
 
 btn.addEventListener('click', addTask);
